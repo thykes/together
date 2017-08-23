866513d16d8aa015f1418b5f861082b79587886e
thykes <tim@timhykes.com>
2017-08-23 01:49:41 -0500

    updated contact page

--- site/controllers/contact.php
+++ site/controllers/contact.php
@@ -1,50 +1,95 @@
 <?php
 
-use Uniform\Form;
+return function($site, $pages, $page) {
 
-return function ($site, $pages, $page)
-{
-    $form = new Form([
-        'filefield' => [
-            'rules' => [
-                'required',
-                'file',
-                'mime' => ['application/pdf'],
-                'filesize' => 5000,
-            ],
-            'message' => [
-                'Please choose a file.',
-                'Please choose a file.',
-                'Please choose a PDF.',
-                'Please choose a file that is smaller than 5 MB.',
-            ],
-        ],
-        'email' => [
-            'rules' => ['required', 'email'],
-            'message' => 'Please enter a valid email address',
-        ],
-        'message' => [
-            'rules' => ['required'],
-            'message' => 'Please enter a message',
-        ],
+  $alert = null;
 
-    ]);
+  if(get('submit')) {
 
-    if (r::is('POST')) {
-        $form->uploadAction(['fields' => [
-            'filefield' => [
-                'target' => kirby()->roots()->content(),
-                'prefix' => false,
-            ],
-        ]]);
+    $data = array(
+      'name'  => get('name'),
+      'email' => get('email'),
+      'text'  => get('text')
+    );
+
+    $rules = array(
+      'name'  => array('required'),
+      'email' => array('required', 'email'),
+      'text'  => array('required', 'min' => 3, 'max' => 3000),
+    );
+
+    $messages = array(
+      'name'  => 'Please enter a valid name',
+      'email' => 'Please enter a valid email address',
+      'text'  => 'Please enter a text between 3 and 3000 characters'
+    );
+
+    // some of the data is invalid
+    if($invalid = invalid($data, $rules, $messages)) {
+      $alert = $invalid;
+
+    // the data is fine, let's send the email
+    } else {
+
+      // create the body from a simple snippet
+      $body  = snippet('contactmail', $data, true);
+
+      // build the email
+      $email = email(array(
+        'to'      => 'tim@timhykes.com',
+        'from'    => $data['email'],
+        'subject' => 'New contact request',
+        'replyTo' => $data['email'],
+        'body'    => $body
+      ));
+
+      // try to send it and redirect to the
+      // thank you page if it worked
+      if($email->send()) {
+        go('contact/thank-you');
+      // add the error to the alert list if it failed
+      } else {
+        $alert = array($email->error());
+      }
+
     }
 
-    if (r::is('POST')) {
-        $form->emailAction([
-            'to' => 'tim@timhykes.com',
-            'from' => 'love@love.com',
-        ]);
-    }
+  }
 
-    return compact('form');
+  return compact('alert');
+
+};
+
+
+
+uniform::$actions['upload'] = function($form, $actionOptions) {
+   foreach ($actionOptions['files'] as $field) {
+      if (!array_key_exists($field, $_FILES)) {
+         return [
+            'success' => false,
+            'message' => "Field {$field} was not submitted"
+         ];
+      }
+   }
+
+   foreach ($actionOptions['files'] as $field) {
+      move_uploaded_file($_FILES[$field]['tmp_name'], $actionOptions['target'].'/'.$_FILES[$field]['name']);
+   }
+
+   return [
+      'success' => true,
+      'message' => "All files uploaded"
+   ];
+};
+
+return function($site, $pages, $page) {
+   $form = uniform('upload-form', [
+      'actions' => [[
+         '_action' => 'upload',
+         'files' => ['file'],
+         'target' => 'content/'.$page->diruri().'/files',
+      ]]
+   ]);
+
+   return compact('form');
 };
